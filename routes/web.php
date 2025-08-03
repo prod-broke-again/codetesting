5b1f5121f4f03668ae5545b1e6898f49d2f4a90d
Eugene <laravelka@yandex.ru>
2025-08-03 19:08:08 +0300

    Fix routing: add catch-all route for Vue SPA in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Главная страница - отдаем Vue приложение
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Роуты для Vue приложения - все остальные запросы отдаем в Vue
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '.*');
